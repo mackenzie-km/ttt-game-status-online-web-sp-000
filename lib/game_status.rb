0ee5c0696182841e7e36b4807945a85fbf16b073
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-19 23:42:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,9 +27,9 @@ board_index_2 = board[winning_index_2]
 board_index_3 = board[winning_index_3]
 
 if board_index_1 == "X" && board_index_2 == "X" && board_index_3 == "X"
-  sub_array
+  return sub_array
 else
-  false
+  return false
 end
 end
 end
