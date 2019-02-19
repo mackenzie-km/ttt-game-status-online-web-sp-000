0d6e8c5f9b741df904a0ace2317c29ae941fcb7d
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-19 23:28:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,21 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+win_combinations = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+  ]
+  
+
+def won?(board)
+array_string = board.join(, )
+if array_string.include?(""X", "X", "X"")
+  return true
+end
+end
