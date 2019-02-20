bb0fcabe0e3d92d26b92cfb8f9c1bcefaa1002b2
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-20 00:25:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,8 @@ WIN_COMBINATIONS = [
   
 
 def won?(board)
-for sub_array in WIN_COMBINATIONS {
-  winning_index_1 = sub_array[0]
+for sub_array in WIN_COMBINATIONS do
+winning_index_1 = sub_array[0]
 winning_index_2 = sub_array[1]
 winning_index_3 = sub_array[2]
 
@@ -31,7 +31,7 @@ board_index_3 = board[winning_index_3]
 else
  false
 end
-}
+end
 end
 
 
@@ -43,4 +43,13 @@ def full?(board)
   end 
 end
 
-
+def draw?(board)
+  if !board.include?(" " || "" || nil) && (won(board) == false)
+    TRUE 
+  elsif won(board) == false && board.include?(" " || "" || nil)
+    FALSE 
+  else
+    FALSE
+end 
+end 
+end  
